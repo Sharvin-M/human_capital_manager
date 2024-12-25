54bcc3ed152355081f6ac49c6a96cb67dd56e90d
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-12-24 20:58:37 -0800

    added badge number data member

--- Employee.hpp
+++ Employee.hpp
@@ -1,35 +1,45 @@
 //
 // Created by Sharvin on 12/24/24.
 //
-//EMPLOYEE_HPP
+// EMPLOYEE_HPP
 #pragma once
 
 class Employee
 {
-    //private data members
+    // data members are encapsulated by default because of class
+    int         badgeNumber;
     int         monthlySalary;
     std::string name;
     std::string contact;
     std::string department;
 
 public:
-    // the constructor is explicit because it has 1 arg
-    explicit Employee(std::string name_)
+
+    // constructor with minimum args for Employee
+    Employee( int badgeNumber_, int monthlySalary_, std::string name_ )
     {
-        name = name_;
+        badgeNumber =   badgeNumber_;
+        monthlySalary = monthlySalary_;
+        name =          name_;
+        contact = "";
+        department = "";
     }
 
-    // constructor that requires all args possible
-    Employee(std::string name_, std::string contact_, std::string department_, int monthlySalary_)
+    // constructor with maximum args for Employee
+    Employee( int badgeNumber_, int monthlySalary_, std::string name_,
+        std::string contact_, std::string department_ )
     {
+        badgeNumber =   badgeNumber_;
+        monthlySalary = monthlySalary_;
         name =          name_;
         contact =       contact_;
         department =    department_;
-        monthlySalary = monthlySalary_;
     }
 
+    // getter methods becuase the data members are encapsulated
+    int getBadgeNumber()            { return badgeNumber; }
+    int getMonthlySalary()          { return monthlySalary; }
     std::string getName()           { return name; }
     std::string getContact()        { return contact; }
     std::string getDepartment()     { return department; }
-    int getMonthlySalary()          { return monthlySalary; }
 };
