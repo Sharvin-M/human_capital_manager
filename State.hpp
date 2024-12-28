49c38fadc01660ed367bc1890f953f78f13716ba
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-12-27 16:46:54 -0800

    created class for state management

---
+++ State.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Sharvin on 12/27/24.
+//
+//STATE_HPP
+
+#pragma once
+
+class State
+{
+    // TODO add event handler methods based on current state
+
+public:
+    State();
+    virtual void welcome();
+    virtual void userInput();
+};
