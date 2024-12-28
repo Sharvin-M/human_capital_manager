510752cf26b92314b8ec1b45607bdf5edf074f36
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-12-27 16:47:13 -0800

    experimenting with smart pointers

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include "Project.hpp"
 #include "Employee.hpp"
-
-// example uses of the current system
+#include "Startup.hpp"
+#include "State.hpp"
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
 
-    Employee Bob(111, 1000, "bob");
 
-    Project Main("finance etl pipeline");
-
-    Main.addMember(Bob, 111 );
-
-    Main.getMember(111);
-
-    Main.getProjectCost();
     return 0;
 }
